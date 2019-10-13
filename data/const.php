7e294f855253c9821b04ac110bc37dc411fa8af2
Tiagelska <tiagelska@gmail.com>
2019-10-13 16:11:02 +0300

    lesson 4

---
+++ data/const.php
@@ -0,0 +1,9 @@
+<?php
+
+define('SITE_ROOT', '../');
+define('WWW_ROOT', SITE_ROOT.'public/');
+define('CSS_ROOT', WWW_ROOT.'css/');
+define('IMG_DIR', WWW_ROOT.'img/');
+define('IMG_PROMO_DIR', WWW_ROOT.'img_promo/');
+
+?>
